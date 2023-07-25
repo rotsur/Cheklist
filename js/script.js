80e37c35c50902a229762b0c5f1805c33f54035e
Roman <romanred99@gmail.com>
2023-07-25 12:17:31 +0200

    finish

--- js/script.js
+++ js/script.js
@@ -8,19 +8,41 @@ input.addEventListener('keypress', function (event) {
         let task = document.createElement('span');
         task.classList.add('task');
         task.textContent = this.value;
+        task.addEventListener('dblclick', function () {
+            let text = this.textContent;
+            this.textContent = '';
+
+            let edit = document.createElement('input');
+            edit.value = text;
+            this.appendChild(edit);
+
+            let self = this;
+            edit.addEventListener('keypress', function (event) {
+                if (event.key == 'Enter') {
+                    self.textContent = this.value;
+                }
+            });
+        });
         li.appendChild(task);
 
         let remove = document.createElement('span');
-        remove.classList.add('remove');
         remove.textContent = 'удалить';
+        remove.classList.add('remove');
+        remove.addEventListener('click', function () {
+            this.parentElement.remove();
+        });
         li.appendChild(remove);
 
         let mark = document.createElement('span');
-        mark.classList.add('mark');
         mark.textContent = 'сделано';
+        mark.classList.add('mark');
+        mark.addEventListener('click', function () {
+            this.parentElement.classList.add('done');
+        });
         li.appendChild(mark);
 
         list.appendChild(li);
+
         this.value = '';
     }
 });
