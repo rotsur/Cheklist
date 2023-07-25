29c10e0281c0d135aa296e14997c4b739f2e3961
Roman <romanred99@gmail.com>
2023-07-25 12:03:39 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+let input = document.querySelector('#input');
+let list = document.querySelector('#list');
+
+input.addEventListener('keypress', function (event) {
+    if (event.key == 'Enter') {
+        let li = document.createElement('li');
+
+        let task = document.createElement('span');
+        task.classList.add('task');
+        task.textContent = this.value;
+        li.appendChild(task);
+
+        let remove = document.createElement('span');
+        remove.classList.add('remove');
+        remove.textContent = 'удалить';
+        li.appendChild(remove);
+
+        let mark = document.createElement('span');
+        mark.classList.add('mark');
+        mark.textContent = 'сделано';
+        li.appendChild(mark);
+
+        list.appendChild(li);
+        this.value = '';
+    }
+});
